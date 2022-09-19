ecc59831f39cc5622e4f07728a67a921619d8dcb
Ilyass-Bougati <ilyass.bougati4ys@gmail.com>
2022-09-19 14:49:09 +0100

    Creating the login and register pages

--- app.py
+++ app.py
@@ -1,13 +1,37 @@
+from crypt import methods
 from flask import *
 from sys import argv
+from helpers import login_required
 
 # creating the app
 app = Flask(__name__)
 
 # the main route
 @app.route("/")
+@login_required
 def index():
     return "Hello world"
+
+
+#the login route
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+
+    elif request.method == "POST":
+        return "post"
+
+
+# The registering route
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+
+    elif request.method == "POST":
+        return "post"
+        
 
 if __name__ == "__main__":
     try:
